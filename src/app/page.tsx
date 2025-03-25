d58670dc949f520eae32e777e1d4e4c3637ec303
SamuelRicardos <samuelric4rdo@gmail.com>
2025-03-25 13:03:08 -0300

    primeiro commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <main>
+    <h1>Projeto dashboard</h1>
+    <Button>
+          Teste button
+    </Button>
+    </main>
+  );
+}
