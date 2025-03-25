3a26754388b21fc8fbbf62b6cb8c75bec8148fdd
SamuelRicardos <samuelric4rdo@gmail.com>
2025-03-25 13:49:50 -0300

    sidebar do mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
   return (
     <main>
-    <h1>Projeto dashboard</h1>
-    <Button>
-          Teste button
-    </Button>
+    <h1>Projeto </h1>
     </main>
   );
 }
